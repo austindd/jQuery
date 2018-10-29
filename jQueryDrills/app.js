2f3eb5970b56fcc479aae8f2d2b285266b91a5ea
Austin Davis <austindanieldavis@gmail.com>
2018-10-29 12:24:17 -0500

    jQuery Drills lab - playing with jQuery uusing Bootstrap and other style tools.

--- jQueryDrills/app.js
+++ jQueryDrills/app.js
@@ -2,26 +2,38 @@ $(document).ready(function() {
 
     console.log('Page Loaded');
 
-    // Bootstrappin' because it's pretty.
+    // Bootstrappin' and flexboxin' because it's pretty.
     $(document.body).css('margin', '2em');
-    $(document.body).prepend("<div id='mainContainer' class='flex-box container'></div>");
+    $(document.body).prepend("<div id='mainContainer' class='container'></div>");
+
     
     // Create rows for the page.
-    $('#mainContainer').append("<div id='row1' class='row'></div>");
-    $('#mainContainer').append("<div id='row2' class='row'></div>");
-    $('#mainContainer').append("<div id='row3' class='row'></div>");
+    $('#mainContainer').append("<div id='row1' class='row flex-centered'></div>");
+    $('#mainContainer').append("<div id='row2' class='row flex-centered'></div>");
+    $('#mainContainer').append("<div id='row3' class='row flex-centered'></div>");
 
+    $('.flex-centered').css({
+        'display': 'flex',
+        'justify-content': 'center',
+    });
 
     // Create form with submit button.
-    $('#row1').append("<form action='' id='form1'></form>");
+    $('#row1').append("<form id='form1' action=''></form>");
     $('#form1').append("<input id='form1TextBox' type='text'></input>");
+    $('#form1').append("<input id='btnSubmit' type='submit' value='Submit' class='btn-dark'></input>");
+
     $('#form1').css({
         'display': 'block',
-        'margin': 'auto'
+        'margin': '2em',
     });
-    $('#form1').append("<input id='btnSubmit' type='submit' value='Submit' class='btn-dark'></input>");
-    $('#btnSubmit').css({'margin': '0', 'border-radius': '0.5em'});
-    $('#form1TextBox').css('border-radius', '0.25em');
+    $('#btnSubmit').css({
+        'margin': '0 0.5em 0 0.5em',
+        'border-radius': '0.5em',
+    });
+    $('#form1TextBox').css({
+        'margin': '0 0.5em 0 0.5em',
+        'border-radius': '0.25em',
+    });
     
 
 
@@ -41,7 +53,6 @@ $(document).ready(function() {
             console.log("Valid Form");
         };
     });
-
     // Trigger form validation when page loads in case of auto-complete.
     $('#form1TextBox').trigger('change')
 
@@ -50,7 +61,17 @@ $(document).ready(function() {
         console.log('Form Submitted');
         console.log($('input').val());
         alert(`${$('input[type=text]').val()}`);
+
+        $('#row2').append(`<div id='messageBanner'></div>`);
+
+        $('#messageBanner').css({
+            'margin': '2em',
+            'font-family': 'Lobster, cursive',
+            'text-align': 'center',
+        });
+        $('#messageBanner').append(`<h3 id='messageText'>${$('input[type=text]').val()}</h3>`);
+
         return false;   // Prevent reload page (form default)
     });
 
-});
+}); // END DOCUMENT.READY FUNCTION.
