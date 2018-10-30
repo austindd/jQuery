a64e0079d65dd966844da87e0f5c135fc5a407bb
Austin Davis <austindanieldavis@gmail.com>
2018-10-30 16:41:05 -0500

    completed objective #5

--- ManipulatingHTML/jQuery/jqueryscript.js
+++ ManipulatingHTML/jQuery/jqueryscript.js
@@ -13,6 +13,18 @@ $(document).ready(function() {
     $("#row3").append(`<div id="column7" class="column"></div>`);
     $("#row3").append(`<div id="column8" class="column"></div>`);
     $("#row3").append(`<div id="column9" class="column"></div>`);
+    $("#mainContainer").append(`<div id="row4" class="row d-flex"></div>`);
+    $("#row4").append(`<div id="column10" class="column"></div>`);
+    $("#row4").append(`<div id="column11" class="column"></div>`);
+    $("#row4").append(`<div id="column12" class="column"></div>`);
+    $("#mainContainer").append(`<div id="row5" class="row d-flex"></div>`);
+    $("#row5").append(`<div id="column13" class="column"></div>`);
+    $("#row5").append(`<div id="column14" class="column"></div>`);
+    $("#row5").append(`<div id="column15" class="column"></div>`);
+    $("#mainContainer").append(`<div id="row6" class="row d-flex"></div>`);
+    $("#row6").append(`<div id="column16" class="column"></div>`);
+    $("#row6").append(`<div id="column17" class="column"></div>`);
+    $("#row6").append(`<div id="column18" class="column"></div>`);
 
     // Main Page Style Template
     $(document.body).css({'margin': '0'});
@@ -30,13 +42,13 @@ $(document).ready(function() {
     });
 
 
+
 // ==================== OBJECTIVE 1 ====================
 
     // Styling row 1
     $("#column1").addClass('col-1 d-inline-flex justify-content-center');
     $("#column2").addClass('col-10 d-inline-flex justify-content-center');
     $("#column3").addClass('col-1 d-inline-flex justify-content-center');
-
     $('#column1').append(`<div id='taskNum1' class='taskNum'>(1)</div>`);
 
     // Button 1
@@ -51,14 +63,12 @@ $(document).ready(function() {
     $("#column4").addClass('col-1 d-inline-flex justify-content-center');
     $("#column5").addClass('col-10 d-inline-flex justify-content-center');
     $("#column6").addClass('col-1 d-inline-flex justify-content-center');
-
     $('#column4').append(`<div id='taskNum2' class='taskNum'>(2)</div>`);
 
     // Text box and button wrapper
     $('#column5').append(`<div id='textBoxWrapper'></div>`);
     $('#textBoxWrapper').css({
         'display': 'block',
-        
     });
 
     // Text Box
@@ -70,8 +80,7 @@ $(document).ready(function() {
         'border': '1px solid black',
         'word-wrap': 'normal',
     });
-
-    $('#textBox').keypress(function(){
+    $('#textBox').keypress(function(){  // Press 'enter' key to trigger alert
         let key = event.keyCode;
         if (key == 13){
             alert(`${$('#textBox').val()}`);
@@ -80,15 +89,132 @@ $(document).ready(function() {
 
     // Button 2
     $('#textBoxWrapper').append(`<button id='button2' class='btn btn-dark align-bottom'>Submit</button>`)
-
     $('#button2').on('click', function(){
         alert(`${$('#textBox').val()}`);
     });
 
 
+// ==================== OBJECTIVE 3 ====================
 
+    $("#column7").addClass('col-1 d-inline-flex justify-content-center');
+    $("#column8").addClass('col-10 d-inline-flex justify-content-center');
+    $("#column9").addClass('col-1 d-inline-flex justify-content-center');
+    $('#column7').append(`<div id='taskNum3' class='taskNum'>(3)</div>`);
+
+    // Div with hover text
+    $('#column8').append(`<div id='hoverDiv' class='bg-light'>Don't Click Me!</div>`);
+    $('#hoverDiv').css({
+        'height': '100px',
+        'width': '162px',
+        'border': '1px solid black',
+        'border-radius': '5%',
+        'text-align': 'center',
+        'padding': '34px 0 0 0',
+        'font-weight': 'bold',
+    });
+
+    $('#hoverDiv').on('mouseenter', function(){
+        let red = Math.floor(Math.random() * 256);
+        let green = Math.floor(Math.random() * 256);
+        let blue = Math.floor(Math.random() * 256);
+        let randomColor = `rgb(${red},${green},${blue})`;
+        $('#hoverDiv').removeClass('bg-light');
+        $('#hoverDiv').css({'background-color': randomColor});
+    });
+    $('#hoverDiv').on('mouseout', function(){
+        $('#hoverDiv').addClass('bg-light');
+        $('#hoverDiv').css({'background-color': ''});
+    });
+    $('#hoverDiv').on('click', function(){
+        window.location.href = `helloworld.txt`;
+    });
+
+
+// ==================== OBJECTIVE 4 ====================
+
+    $("#column10").addClass('col-1 d-inline-flex justify-content-center');
+    $("#column11").addClass('col-10 d-inline-flex justify-content-center');
+    $("#column12").addClass('col-1 d-inline-flex justify-content-center');
+    $('#column10').append(`<div id='taskNum4' class='taskNum'>(4)</div>`);
+
+    // Paragraph 1
+    $('#column11').append(`<p id='para1' class='para'></p>`);
+    $('#para1').text("Pi Draconis, Latinized from π Dra, is a solitary star in the northern circumpolar constellation of Draco. It is visible to the naked eye with an apparent visual magnitude of 4.59. Based upon an annual parallax shift of 14.25 mas as measured from Earth, it is located around 229 light years from the Sun. At that distance, the visual magnitude is diminished by an extinction factor of 0.063±0.10 due to interstellar dust.");
+    $('#para1').css({
+        'text-align': 'justify',
+        'text-justify': 'inter-word',
+    });
+
+    // Paragraph change color
+    $('#para1').on('click', function(){
+        let red = Math.floor(Math.random() * 256);
+        let green = Math.floor(Math.random() * 256);
+        let blue = Math.floor(Math.random() * 256);
+        let randomColor = `rgb(${red},${green},${blue})`;
+        $('#para1').removeClass('bg-light');
+        $('#para1').css({'color': randomColor});
+    });
 
     
+// ==================== OBJECTIVE 5 ====================
+
+    $("#column13").addClass('col-1 d-inline-flex justify-content-center');
+    $("#column14").addClass('col-10 d-inline-flex justify-content-center');
+    $("#column15").addClass('col-1 d-inline-flex justify-content-center');
+    $('#column13').append(`<div id='taskNum5' class='taskNum'>(5)</div>`);
+
+    // Wrapper Div (button3, nameBanner)
+    $('#column14').append(`<div id='nameBannerWrapper' class='flex-column justify-content-center'></div>`);
+    $('#nameBannerWrapper').css({
+        'display': 'block',
+    });
+    // Button 3
+    $('#nameBannerWrapper').append(`<button id='button3' class='btn btn-dark'>Don't wear it out!</button>`);
+    $('#button3').css({
+        'display': 'block',
+        'margin': '0 auto',
+    });
+    // Name Banner
+    $('#nameBannerWrapper').append(`<div id='nameBanner'></div>`);
+    $('#nameBanner').css({
+        'height': '100px',
+        'width': '400px',
+        'border': '1px solid black',
+        'margin': '0 auto',
+        'text-align': 'center',
+        'padding': '14px',
+    });
+    // Button3 Function
+
+    let nameBannerFlag = false;
+
+    $('#button3').on('click', function(){
+        if(nameBannerFlag == false){
+            $('#nameBanner').text("Austin Davis");
+            $('#nameBanner').addClass('display-4 bg-warning')
+            nameBannerFlag = true;
+        } else if(nameBannerFlag == true){
+            $('#nameBanner').text("");
+            $('#nameBanner').removeClass('display-4 bg-warning')
+            nameBannerFlag = false;
+        };
+
+    });
+      
+    
+
+
+
+// ==================== OBJECTIVE 6 ====================
+
+    $("#column16").addClass('col-1 d-inline-flex justify-content-center');
+    $("#column17").addClass('col-10 d-inline-flex justify-content-center');
+    $("#column18").addClass('col-1 d-inline-flex justify-content-center');
+    $('#column16').append(`<div id='taskNum6' class='taskNum'>(6)</div>`);
+
+
+// =====================================================
+
     // Class Styles
     $('.taskNum').css({
         'padding': '0',
