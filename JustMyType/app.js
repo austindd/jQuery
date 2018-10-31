4aeb8b9ac5582be245b46711d32ed375a167e138
Austin Davis <austindanieldavis@gmail.com>
2018-10-31 18:03:20 -0500

    I'm now an expert on key codes and character codes...

--- JustMyType/app.js
+++ JustMyType/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    Press & hold 'shift' to toggle lower/upper keyboard
+    // Press & hold 'shift' to toggle lower/upper keyboard
 
     $('#keyboard-upper-container').hide();
     let shiftKeyDown = false;
@@ -14,7 +14,7 @@ $(document).ready(function () {
         };
     });
     $(document).keyup(function () {
-        let key = event.keyCode;
+        let key = event.keyCode || event.which;
         if (key == 16) {
             $('#keyboard-upper-container').hide();
             $('#keyboard-lower-container').show();
@@ -22,102 +22,65 @@ $(document).ready(function () {
         };
     });
 
-    //============================= Working Copy ====================================
+
+    let sentences = [
+        'ten ate neite ate nee enet ite ate inet ent eate', 
+        'Too ato too nOt enot one totA not anot tOO aNot', 
+        'oat itain oat tain nate eate tea anne inant nean', 
+        'itant eate anot eat nato inate eat anot tain eat', 
+        'nee ene ate ite tent tiet ent ine ene ete ene ate'
+    ];
+    
+    $('#sentence').append(`<p id='para1' class='output-line'></p>`);
+    $('#sentence').append(`<p id='para2' class='output-line'></p>`);
+    $('#sentence').append(`<p id='para3' class='output-line'></p>`);
+    $('#sentence').append(`<p id='para4' class='output-line'></p>`);
+    $('#sentence').append(`<p id='para5' class='output-line'></p>`);
+
+
+
+
+// =================== KEYBOARD & OUPUT LOGIC ===================
 
     let myCharCode = null;
+    let currentRow = 1;
+    let currentString = '';
+
+    let sentenceObject = {
+        1: [''],
+        2: [''],
+        3: [''],
+        4: [''],
+        5: [''],
+    };
 
     $(document.body).keypress(function(){
-        myCharCode = event.keyCode;
-        console.log(event.keyCode);
+        myCharCode = event.keyCode || event.which;
+        myChar = event.key;
+        console.log('Key:', event.key, 'ASCII: ', event.keyCode);
         $(`#${myCharCode}`).addClass('well-highlight');
-    });
 
-    $(document.body).keydown(function(){
-        console.log('Key Down: ', myCharCode);
-    });
+        if (myCharCode == 13) {
+            currentRow = currentRow + 1;
+        } else if (myCharCode != 13) {
+            currentString = sentenceObject[currentRow];
+            let newString = currentString + myChar;
+            sentenceObject[currentRow] = newString;
+    
+            $('#para1').text(sentenceObject[1]);
+            $('#para2').text(sentenceObject[2]);
+            $('#para3').text(sentenceObject[3]);
+            $('#para4').text(sentenceObject[4]);
+            $('#para5').text(sentenceObject[5]);
+        };
+        });
+
+        
+
+
 
     $(document.body).keyup(function(){
-        console.log('Key Up: ', myCharCode);
-        $(`#${myCharCode}`).removeClass('well-highlight');
+        $(`.well-highlight`).removeClass('well-highlight');
     });
-
-
-
-
-
-
-
-
-
-    
-
-
-// ======================== Attempts to handle multiple keypresses at once.
-
-//     let myCharCode = null;
-//     let myKeyCode = null;
-//     let charCodeArray = [null];
-//     let keyCodeArray = [null];
-//     let keyIndex = 0;
-
-// // ============================ KEY PRESSED ============================
-//     $(document.body).keypress(function () {
-//         myCharCode = event.keyCode;
-//         charCodeArray[keyIndex] = event.keyCode;
-//         console.log('charCodeArray: ', charCodeArray);
-//         for (let i = 0; i < keyIndex; function () {
-//             $(`#${charCodeArray[i]}`).addClass('well-highlight');
-//         });
-//         keyIndex = keyIndex + 1;
-//     });
-// // ============================ HOLD KEY DOWN ============================
-//     $(document.body).keydown(function () {
-//         myKeyCode = event.keyCode;
-//         keyCodeArray[keyIndex] = event.keyCode;
-//         console.log('Key Down: ', myCharCode);
-//     });
-// // ============================ LIFT KEY UP ============================
-//     $(document.body).keyup(function () {
-//         console.log('keyCodeArray: ', keyCodeArray);
-//         $(`#${myKeyCode}`).removeClass('well-highlight');
-//     });
-
-
-
-
-    //=================================================================
-
-    // $(document.body).keydown(function(){
-
-    //     let key = (event.keyCode);
-
-    //     if (shiftKeyDown == false && key > 64 && key < 91){
-    //         key = key + 32;     // Accounting for lowercase ASCII codes (JS keyCode +32)
-    //         $(`#${key}`).addClass('well-highlight');
-    //     } else {
-    //         $(`#${key}`).addClass('well-highlight');
-    //     };
-    // });
-
-    // $(document.body).keyup(function(){
-
-    //     let key = (event.keyCode);
-
-    //     if (shiftKeyDown == false && key > 64 && key < 91){
-    //         key = key + 32;     // Accounting for lowercase letter ASCII codes (JS keyCode +32)
-    //         console.log('keyup: ', key);
-    //         console.log($(`#${key}`));
-    //         $(`#${key}`).removeClass('well-highlight');
-    //     } else {
-
-    //         console.log('keyup: ', key);
-    //         console.log($(`#${key}`));
-    //         $(`#${key}`).removeClass('well-highlight');
-    //     };
-    // });
-
-
-
-
 
 });
